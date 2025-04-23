3eef0ddd56c046915c7ce0d63ca5cf5b288bd202
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-23 14:45:52 +0200

    feat: 2nd way to create context and hook

--- src/contexts/PostsContext.jsx
+++ src/contexts/PostsContext.jsx
@@ -1,6 +1,32 @@
-
-import { createContext } from "react";
+//vado ad importare tutto ciò che mi serve per creare un context APi Pattern
+import { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
 
 const PostContext = createContext();
 
-export default PostContext;
+//creo la funzione/componente per delegare tutto ciò che serve per ottenere le informazioni dei post
+function PostsProvider({ children }) {
+
+    const urlApi = 'https://jsonplaceholder.typicode.com/posts';
+    const [postList, setPostList] = useState([]);
+
+    useEffect(() => {
+
+        axios.get(urlApi)
+            .then(res => setPostList(res.data))
+            .catch(err => console.log(err))
+
+    }, [])
+
+    return <PostContext.Provider value={{ postList, setPostList }}>
+        {children}
+    </PostContext.Provider>
+}
+
+//creo il mio hook per i posts
+function usePosts() {
+    const context = useContext(PostContext);
+    return context;
+}
+
+export { PostsProvider, usePosts };
