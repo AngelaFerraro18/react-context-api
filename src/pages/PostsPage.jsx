2e62637e0bd94b24706e9d572261e2f4d36c2d1f
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-23 16:07:50 +0200

    feat: import Alert component to show it only in PostsPage

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,7 +1,10 @@
 import PostsList from "./PostsList";
+import Alert from "../components/Alert";
 
 function PostsPage() {
+
     return <>
+        <Alert />
         <h2>Lista dei posts:</h2>
         <PostsList />
     </>
