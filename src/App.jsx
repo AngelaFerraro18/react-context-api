2e34decb6940086c3546f2c95eccddac5a84d3d5
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-23 14:34:18 +0200

    feat: create PostsContext and import in App

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,40 @@ import DefaultLayout from "./layouts/DefaultLayout"
 import SinglePost from "./pages/SinglePost"
 import NotFound from "./pages/NotFound"
 import PostsPage from "./pages/PostsPage"
+import PostContext from "./contexts/PostsContext"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
 
+  const urlApi = 'https://jsonplaceholder.typicode.com/posts';
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    console.log('Ho caricato la pagina')
+
+    axios.get(urlApi)
+      .then(res => setPostList(res.data))
+      .catch(err => console.log(err))
+
+  }, [])
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path='/' element={<Home />} />
-            <Route path='/post-list' element={<PostsPage />} />
-            <Route path='/post-list/:id' element={<SinglePost />} />
-            <Route path='/chi-siamo' element={<ChiSiamo />} />
-            <Route path='*' element={<NotFound />} />
-          </Route>
-        </Routes>
+      <PostContext.Provider value={{ postList, setPostList }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path='/' element={<Home />} />
+              <Route path='/post-list' element={<PostsPage />} />
+              <Route path='/post-list/:id' element={<SinglePost />} />
+              <Route path='/chi-siamo' element={<ChiSiamo />} />
+              <Route path='*' element={<NotFound />} />
+            </Route>
+          </Routes>
 
-      </BrowserRouter>
+        </BrowserRouter>
+      </PostContext.Provider>
     </>
   )
 }
