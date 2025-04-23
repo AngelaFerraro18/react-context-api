48d295f2c06b5e65ef8854efe6b93a30528de25d
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-23 16:31:20 +0200

    feat: add fn to set a timer to unshow Alert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,12 +1,32 @@
 
+import { useEffect, useState } from "react";
 import { useAlert } from "../contexts/AlertContext";
 
 
 function Alert() {
     const { alert } = useAlert();
 
+    //creo una variabile di stato per gestire il comportamento dell'alert
+    const [showAlert, setShowAlert] = useState(true);
+
+    //creo una funzione per far sparire l'alert dopo 5s
+    function showAlertFn() {
+        const timer = setTimeout(() => {
+            setShowAlert(false);
+        }, 5000);
+
+        //creo una funzione per ripulire il timer
+        function clearTimer() {
+            return clearTimeout(timer);
+        }
+        return clearTimer;
+    }
+
+    //uso useEffect per far comparire l'alert quando il componente viene montato
+    useEffect(showAlertFn, []);
+
     return <>
-        {alert.message && <p><strong>{alert.type}:</strong> {alert.message}</p>}
+        {showAlert && alert.message && (<p><strong>{alert.type}:</strong> {alert.message}</p>)}
     </>
 }
 
